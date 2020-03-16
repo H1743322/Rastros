ec1a8c558040b5d2c15603431926a8f3242412ee
Francisco Paulino <61980698+Francis-08@users.noreply.github.com>
2020-03-16 17:47:41 +0000

    Add files via upload

--- v2/main.c
+++ v2/main.c
@@ -6,8 +6,20 @@ int main()
 {
     ESTADO *e = inicializar_estado();
 
-    interpretador(e);
+    mostrar_tabuleiro(e);
 
+    while (game_over(e)!=1)
+    {
+        prompt(e);
+
+        interpretador(e);
+
+        
+    }
+    if(obter_estado_casa(e, (COORDENADA){0, 7}) == BRANCA)
+        printf("JOGADOR 1 VENCEU !!");
+    else
+        printf("JOGADOR 2 VENCEU !!");
     system("pause");
     return 0;
 }
